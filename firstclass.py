b942260f25ccc0b2682dece8629cb8072b8d1287
Ralu <ralu2K3@gmail.com>
2022-01-18 15:51:53 +0100

    variables, values, expressions, operators

---
+++ firstclass.py
@@ -0,0 +1,47 @@
+# print('This is Astro-verse')
+
+
+# print(type(int('420')))
+
+# str = 'hello world'
+# int = (2022)
+# float = (2.0)
+
+# print(str)
+# print(int)
+# print(float)
+
+# x = (-10 +(60**0.5)) /10
+
+# print(x)
+
+
+# mile1= (8 * 60 * 2) + 30
+# mile2= (7 * 60 *3) +36
+
+# total_seconds = mile1 + mile2 
+
+# time_home =  (total_seconds)/ 60
+
+# eta = time_home 
+# print (time_home)
+# print (total_seconds)
+
+
+book_price = 24.95
+
+first_sale = ((0.6 * book_price) + 3)
+remaining_sale = ((0.6 * book_price) + 0.75) *59
+
+whole_sale = first_sale + remaining_sale 
+
+print(whole_sale)
+
+# area = 20
+# carpet_cost = 24.99
+# cushion_cost = 2.99
+# labor_cost = 18
+
+# total_cost = (carpet_cost * 20) + (cushion_cost * 20) + (labor_cost * 20) + (35)
+
+# print(total_cost)
